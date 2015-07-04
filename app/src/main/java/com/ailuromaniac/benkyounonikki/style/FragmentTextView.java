ba7f89a737548169a439500be8402a0affaf1396
aneesa <a_aneesa@yahoo.com>
2015-07-04 09:15:41 +0800

    Added bordered textview

--- app/src/main/java/com/ailuromaniac/benkyounonikki/style/FragmentTextView.java
+++ app/src/main/java/com/ailuromaniac/benkyounonikki/style/FragmentTextView.java
@@ -18,11 +18,13 @@ public class FragmentTextView extends TextView {
     private static final String FRAGMENT_SECTION_TITLE_TEXTVIEW = "FragmentSectionTitleTextView";
     private static final String FRAGMENT_CENTER_TEXTVIEW = "FragmentCenterTextView";
     private static final String FRAGMENT_LEFT_TEXTVIEW = "FragmentLeftTextView";
+    private static final String FRAGMENT_BORDERED_TEXTVIEW = "FragmentBorderedTextView";
     private static final String HIRAGANA_TEXTVIEW = "HiraganaTextView";
     private static final String KATAKANA_TEXTVIEW = "KatakanaTextView";
     private static final String ROMAJI_TEXTVIEW = "RomajiTextView";
 
     private static final int DRAWABLE_FRAGMENT_HEADER_TEXTVIEW = R.drawable.fragment_header_textview;
+    private static final int DRAWABLE_FRAGMENT_BORDERED_TEXTVIEW = R.drawable.fragment_bordered_textview;
     private static final int DRAWABLE_HIRAGANA_TEXTVIEW = R.drawable.hiragana_textview;
     private static final int DRAWABLE_KATAKANA_TEXTVIEW = R.drawable.katakana_textview;
     private static final int DRAWABLE_ROMAJI_TEXTVIEW = R.drawable.romaji_textview;
@@ -42,8 +44,8 @@ public class FragmentTextView extends TextView {
         // align the text to center
         this.setGravity(Gravity.CENTER);
 
-        // set padding to 3dp
-        int padding = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 3,
+        // set padding to 10dp
+        int padding = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10,
                 getResources().getDisplayMetrics()));
         this.setPadding(padding, padding, padding, padding);
 
@@ -96,11 +98,30 @@ public class FragmentTextView extends TextView {
         if (style.equalsIgnoreCase(FRAGMENT_LEFT_TEXTVIEW)) {
             // align the text to left
             this.setGravity(Gravity.LEFT);
+        } else if (style.equalsIgnoreCase(FRAGMENT_BORDERED_TEXTVIEW)) {
+            this.setFragmentBorderedTextview(margins, textSize);
         } else if (style.equalsIgnoreCase(HIRAGANA_TEXTVIEW) ||
                     style.equalsIgnoreCase(KATAKANA_TEXTVIEW) ||
                     style.equalsIgnoreCase(ROMAJI_TEXTVIEW)) {
             this.setJapaneseTextview(style, margins, textSize);
         }
+    }
+
+    private void setFragmentBorderedTextview(int margins, float textSize){
+        // set layout
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1.0f);
+
+        // set default textview margins to 5dp
+        layoutParams.setMargins(0, margins, 0, margins);
+        layoutParams.gravity = Gravity.CENTER;
+        this.setLayoutParams(layoutParams);
+
+        // set text size to 25dp
+        this.setTextSize(textSize);
+
+        // set the background shape
+        this.setBackgroundResource(DRAWABLE_FRAGMENT_BORDERED_TEXTVIEW);
     }
 
     private void setJapaneseTextview(String style, int margins, float textSize) {
@@ -114,9 +135,6 @@ public class FragmentTextView extends TextView {
 
         // set text size to 25dp
         this.setTextSize(textSize);
-
-        // align the text to center
-        this.setGravity(Gravity.CENTER);
 
         // set the background shape
         if(style.equalsIgnoreCase(HIRAGANA_TEXTVIEW)) {
